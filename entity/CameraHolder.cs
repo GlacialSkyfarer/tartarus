61209d9dd4b864ba94a48093f6d092e584d6519b
GlacialSkyfarer <63227267+GlacialSkyfarer@users.noreply.github.com>
2026-02-06 11:55:20 -0700

    added rotationtarget to camera holder

--- entity/CameraHolder.cs
+++ entity/CameraHolder.cs
@@ -3,18 +3,23 @@ using System;
 
 public partial class CameraHolder : Node3D
 {
-	[Export] private NodePath p_target;
-	private Node3D _target;
+	[Export] private NodePath p_positionTarget;
+	private Node3D _positionTarget;
+	[Export] private NodePath p_rotationTarget;
+	private Node3D _rotationTarget;
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_target = GetNode<Node3D>(p_target);
+		_positionTarget = GetNode<Node3D>(p_positionTarget);
+		_rotationTarget = GetNode<Node3D>(p_rotationTarget);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GlobalPosition = GlobalPosition.Lerp(_target.GlobalPosition, (float)delta * 16f); 
+		GlobalPosition = GlobalPosition.Lerp(_positionTarget.GlobalPosition, (float)delta * 16f);
+		GlobalRotation = _rotationTarget.GlobalRotation;
 	}
 }
